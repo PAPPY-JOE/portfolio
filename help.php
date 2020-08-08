2e7197b128f79612c812fc88b88961422ff9fb50
PAPPY-JOE <fatoyejoseph@gmail.com>
2020-08-08 04:38:51 +0100

    Made all php html

--- help.php
+++
@@ -1,3 +0,0 @@
-<?php
-    include_once('includes/include.php')
-?>
--- joseph-fatoye/scripts/onload.js
+++
@@ -1,9 +0,0 @@
-
- function onload() {
-    setTimeout(() => {
-        // document.body.querySelector('#onload').style.opacity = ".7";
-        document.body.querySelector('#onload').style.display = "none";
-
-    }, 500);
-
-}
