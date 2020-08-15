44d038592cad2ee74d485a065da14a50a4ea9e90
PAPPY-JOE <fatoyejoseph@gmail.com>
2020-08-15 04:02:30 +0100

    Finished work on appointment.html

--- scripts/onload.js
+++ scripts/onload.js
@@ -1,13 +1,8 @@
 
  function onload() {
     setTimeout(() => {
-        // document.body.querySelector('#onload').style.opacity = ".7";
         document.body.querySelector('#onload').style.display = "none";
 
     }, 500);
 
-    setTimeout(() => {
-        alert('"Book An Appointment" page is now ready, and can be viewed. Visit get started to view')
-
-    }, 5000);
 }
